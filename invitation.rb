e238ec81faf337ad23563f9f3c2527cf11581690
Sara Kim <sarakim0903@gmail.com>
2018-07-11 13:12:27 +0000

    Done.

--- invitation.rb
+++ invitation.rb
@@ -6,10 +6,7 @@ date = "October 31"
 time = "6pm"
 host_name = "Harry Potter"
 
-# party_name = gets.chomp
-# date = gets.chomp
-# time = gets.chomp
-# host_name = gets.chomp
 
 # Try starting out with puts'ing a string.
-puts "Dear #{guest_name}, You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than Oct. 30.  Sincerely,  #{host_name}"
+puts "Dear #{guest_name}, 
+You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than Oct. 30. Sincerely, #{host_name}"
